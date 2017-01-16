6689bb51e20c6264fb0dd10ab39641f7b7ccbdd4
Joshua Scott <jscott313@gmail.com>
2017-01-16 15:25:51 +0200

    Exercise 1

---
+++ ExerciseA1/src/Assignment1.c
@@ -0,0 +1,20 @@
+/*
+ Name: Assignment1.c
+ Author: Joshua Scott
+ Description: Prints out basic information about each group member
+ */
+
+#include<stdio.h>
+
+int main (void)
+{
+	printf("Last name:\t\t\tScott\n"
+			"First name:\t\t\tJoshua\n"
+			"Student number:\t\t\t1503969\n"
+			"Programming experience:\t\tC#, C (basics only)\n"
+			"Motivation to learn C:\t\tHIGH\n"
+			"Grade I am at:\t\t\t1 (but, soon 5!)\n"
+			"My plan to achieve that grade:\tPractise programming on a daily basis!");
+
+			return 0;
+}
---
+++ ExerciseA2/src/Assignment2.c
@@ -0,0 +1,31 @@
+/*
+ Name: Assignment2.c
+ Author: Joshua Scott
+ Description: A program that can count (sum, difference, product, division and power).
+ */
+
+#include<stdio.h>
+#include<math.h>
+
+int main (void)
+{
+	// Variable declarations
+	const int firstNumber = 9;
+	const int secondNumber = 4;
+	int result = 0;
+
+	printf("First number:\t%d\n", firstNumber);
+	printf("Second number:\t%d\n", secondNumber);
+
+	// We can store the result in the variable 'result' and then print, like so:
+	result = firstNumber + secondNumber;
+	printf("Sum:\t\t%d\n", result);
+
+	// Or just embed the calculation within the printf arguments:
+	printf("Difference:\t%d\n", firstNumber - secondNumber);
+	printf("Product:\t%d\n", firstNumber * secondNumber);
+	printf("Division:\t%d\n", firstNumber / secondNumber);
+	printf("Power:\t\t%.0f\n", pow(firstNumber, secondNumber));
+
+	return 0;
+}
