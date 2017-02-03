1f65f13047b8530a42bf4f99b01c74dee38d9a8e
Joshua Scott <jscott313@gmail.com>
2017-02-03 19:17:17 +0200

    Updated Ex4Advanced (still unfinished though)

--- Exercise4Advanced/src/Assignment1.c
+++ Exercise4Advanced/src/Assignment1.c
@@ -12,15 +12,88 @@ You shall be able to print out the element's information
 You shall be able to print out the elements that currently exist periodic table
 You shall be able to empty the whole list and free the memory
 Use pointers and functions and allocate memory dynamically for each element
-*/
+ */
 
-#include <stdio.h>
-#include <stdlib.h>
+#define NAME_LENGTH 20
 
-// Prototypes
+#include<stdio.h>
+#include<stdlib.h>
 
-
-int main (void)
+struct Node
 {
+    char* name[NAME_LENGTH];    // e.g. Hydrogen
+    char* chemicalSymbol[2];    // e.g. H or Ag
+	short int atomicNumber;     // e.g. 1
+	char state;                 // s, l, g or u (unknown)
+
+	struct Node* next;
+	struct Node* prev;
+};
+
+struct Node* head; // global variable - pointer to head node.
+
+//Creates a new Node and returns pointer to it.
+struct Node* GetNewNode(char* name[NAME_LENGTH], char* chemicalSymbol[2],
+                        short int atomicNumber, char state)
+{
+	struct Node* newNode
+		= (struct Node*)malloc(sizeof(struct Node));
+	newNode->name = name;
+	newNode->chemicalSymbol = chemicalSymbol;
+	newNode->atomicNumber = atomicNumber;
+	newNode->state = state;
+
+	newNode->prev = NULL;
+	newNode->next = NULL;
+	return newNode;
+}
+
+//Inserts a Node at head of doubly linked list
+void InsertAtHead(int x)
+{
+	struct Node* newNode = GetNewNode(x);
+	if(head == NULL) {
+		head = newNode;
+		return;
+	}
+	head->prev = newNode;
+	newNode->next = head;
+	head = newNode;
+}
+
+//Inserts a Node at tail of Doubly linked list
+void InsertAtTail(int x)
+{
+	struct Node* temp = head;
+	struct Node* newNode = GetNewNode(x);
+	if(head == NULL) {
+		head = newNode;
+		return;
+	}
+	while(temp->next != NULL) temp = temp->next; // Go To last Node
+	temp->next = newNode;
+	newNode->prev = temp;
+}
+
+//Prints all the elements in linked list in forward traversal order
+void Print()
+{
+	struct Node* temp = head;
+	printf("Forward: ");
+	while(temp != NULL) {
+		printf("%d ",temp->data);
+		temp = temp->next;
+	}
+	printf("\n");
+}
+
+int main()
+{
+
+	/*Driver code to test the implementation*/
+	head = NULL; // empty list. set head as NULL.
+
+	// Calling an Insert and printing list both in forward as well as reverse direction.
+
 
 }
