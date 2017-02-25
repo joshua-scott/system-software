413afd5a54aab49219a10fb61c95c92cf2f06432
Joshua Scott <jscott313@gmail.com>
2017-02-25 20:24:00 +0200

    Added Ex6Advanced, slightly modified Ex6Basic
    Both are now finished

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/fa/0081c01984fb00161d27a0cf2c337fd9
@@ -0,0 +1,40 @@
+/*
+ Name: 			Exercise6Basic, Assignment1.c
+ Author: 		Joshua Scott
+ Description:
+ A command line program that takes one or more arguments and creates an acronym.
+ Any uppercase letters will go into the acronym, lowercase letters will not.
+*/
+
+#include <stdio.h>
+#include <ctype.h>
+
+int main (int argc, char* argv[])
+{
+    // to hold the output, 20 chars should be enough for an acryonym
+    char result[20];
+
+    // Check for insufficient arguments
+    if (argc < 2)
+        printf("Please give at least one word.\n");
+    else
+    {
+        int outputLetter = 0;   // track which letter we're outputting
+        // loop from first word to the last
+        for(int currentWord = 1; currentWord < argc; currentWord++)
+        {
+            // loop through each letter of the current word
+            for(int currentLetter = 0; argv[currentWord][currentLetter] != '\0'; currentLetter++)
+            {
+                // if this letter is uppercase, add it to the acronym
+                if (isupper(argv[currentWord][currentLetter]))
+                {
+                    result[outputLetter] = argv[currentWord][currentLetter];
+                    outputLetter++;
+                }
+            }
+        }
+    }
+    // Output the result
+    printf("%s\n", result);
+}
---
+++ Exercise6Advanced/src/Assignment1.c
@@ -0,0 +1,67 @@
+/*
+ Name: 			Exercise6Advanced, Assignment1.c
+ Author: 		Joshua Scott
+ Description:
+ A command line program that takes one or more arguments and creates an acronym.
+ Any uppercase letters will go into the acronym, lowercase letters will not.
+ A warning will be given if there are any non-alpha characters.
+ Acronyms will be appended to the file 'output.c'
+*/
+
+// QUESTION: IS 'argv[currentWord][currentLetter]' error-proof? if the array is guaranteed to be contiguous, I guess so?
+
+#include <stdio.h>
+#include <ctype.h>
+
+void writeToFile(char* acronym);
+
+int main (int argc, char* argv[])
+{
+    // to hold the output, 20 chars should be enough for an acronym
+    char result[20];
+
+    // check for insufficient arguments
+    if (argc < 2)
+        printf("Please give at least one word.\n");
+    else
+    {
+        int outputLetter = 0,   // track which letter we're outputting
+            alreadyWarned = 0; // track whether we've already warned that non-alpha chars are ignored
+        // loop from first word to the last
+        for(int currentWord = 1; currentWord < argc; currentWord++)
+        {
+            // loop through each letter of the current word
+            for(int currentLetter = 0; argv[currentWord][currentLetter] != '\0'; currentLetter++)
+            {
+                // if there's a non-alpha char, warn that it'll be ignored
+                if (!isalpha(argv[currentWord][currentLetter]) && !alreadyWarned)
+                {
+                    printf("Note that non-alpha characters such as '%c' will be ignored.\n", argv[currentWord][currentLetter]);
+                    alreadyWarned = 1;
+                }
+                // if this letter is uppercase, add it to the acronym
+                else if (isupper(argv[currentWord][currentLetter]))
+                {
+                    result[outputLetter] = argv[currentWord][currentLetter];
+                    outputLetter++;
+                }
+            }
+        }
+        // Output the result and pass it to write to file
+        printf("%s\n", result);
+        writeToFile(result);
+    }
+}
+
+void writeToFile(char* acronym)
+{
+    // open or create the file
+    FILE *filePointer;
+    filePointer = fopen("output.txt", "a"); // we will append to the end of the file, so previous contents won't be overridden
+
+    // write to the file. passing acronym as an argument to a formatted string fixes the clang error below:
+    // "format string is not a string literal (potentially insecure) [-Werror,-Wformat-security]"
+    fprintf(filePointer, "%s\n", acronym);
+
+    fclose(filePointer);
+}
