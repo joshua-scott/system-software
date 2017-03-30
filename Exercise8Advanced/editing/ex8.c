ff4f9a7cfdfd8088ce45ee0bf31b2641879efa48
Joshua Scott <jscott313@gmail.com>
2017-03-30 14:03:32 +0300

    ex8.c skeleton structure now in place. compiles and checks arguments

--- Exercise8Advanced/editing/ex8.c
+++ Exercise8Advanced/editing/ex8.c
@@ -3,32 +3,45 @@
  Author: 		Joshua Scott, Robin Jacobs
  Description:
  A command line program that encrypts and decrypts input to/from a specified file using the XOR operator.
- Usage: ./ex8 [decrypt|encrypt] [$filename] [$password]
+ Usage: ./ex8 [e|d] [$filename] [$password]
 */
-
 
 #include <stdio.h>
 
-int main (int argc, char **argv)
+void encrypt(char* filename, char* password);
+void decrypt(char* filename, char* password);
+
+int main (int argc, char** argv)
 {
 	// If incorrect args, complain
-	if (argc != 4)
-	{
+	if (argc != 4){
 		printf("Insufficient arguments.");
 		return -1;
 	}
+
 	char selectedMode = argv[1][0];
-	if (selectedMode == 'c' || selectedMode == 'e' ||
-		selectedMode == 'C' || selectedMode == 'E')
-	{
-		encrypt(argv);
+	char* filename = argv[2];
+	char* password = argv[3]; 
+
+	switch (selectedMode){
+
+		case 'e':
+			encrypt(filename, password);
+			break;
+		case 'd':
+			decrypt(filename, password);
+			break;
+		default :
+			printf("Second argument should be e (encrypt), or d (decrypt)");
+			return -1;
 	}
-	else if (selectedMode == 'd' || selectedMode == 'D')
-	{
-		decrypt(argv);
-	}
-	else 
-	{
-		printf("Second argument should be 'crypt'/'encrypt', or 'decrypt'");
-	}
+}
+
+void encrypt(char* filename, char* password){
+	printf("encrypt\n");
+	
+}
+
+void decrypt(char* filename, char* password){
+	printf("decrypt\n");
 }
